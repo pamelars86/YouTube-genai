5278d12f25c134f0e53efaf54e00654e52a9cd11
Carmen Pamela Rosales Sedano <pamela.rosales@mercadolivre.com>
2025-02-01 22:10:56 -0300

    feat: single endpoint

--- app/router.py
+++ app/router.py
@@ -1,22 +1,12 @@
 from flask import jsonify
 from app.services.youtube_service import get_video_transcript, get_youtube_video, get_all_youtube_videos
-from app.services.summary_service import summarize_transcript, generate_blog_post
+from app.services.summary_service import generate_blog_post
 from . import app
+import time
 
 @app.route('/')
 def index():
     return "Welcome to the YouTube Summary App!"
-
-# Route to get a summary of a video
-@app.route('/video_summary/<video_id>', methods=['GET'])
-def video_summary(video_id):
-    video_info = get_youtube_video(video_id, withAPIKey=True)
-    transcript = get_video_transcript(video_id)
-    video_info["transcript"] = transcript
-
-    summary = summarize_transcript(transcript, video_info["title"], video_info["description"])
-
-    return jsonify({"video_info": video_info, "summary": summary})
 
 # Route to get all YouTube videos
 @app.route('/all_videos', methods=['GET'])
@@ -28,9 +18,13 @@ def all_videos():
 # Route to get a blog post of a video
 @app.route('/blog_post/<video_id>', methods=['GET'])
 def blog_post(video_id):
+    start_time = time.time()
+
     video_info = get_youtube_video(video_id, withAPIKey=True)
     transcript = get_video_transcript(video_id)
-    video_info["transcript"] = transcript
     blog_post = generate_blog_post(transcript, video_info["title"], video_info["description"])
 
-    return jsonify({"video_info": video_info, "blog_post": blog_post})
+    execution_time = (time.time() - start_time) / 60
+    print(f"[blog_post] Execution time: {execution_time} minutes")
+    return jsonify({"video_info": video_info, "transcript": transcript, "blog_post": blog_post})
+
--- app/services/summary_service.py
+++ app/services/summary_service.py
@@ -1,6 +1,14 @@
 import os
 from app import logger
 import ollama
+import tempfile
+import re
+from dotenv import load_dotenv
+
+
+transcript_dir = '/app/transcripts'
+load_dotenv()
+MODEL_TO_USE = os.getenv("MODEL_TO_USE")
 
 def extract_response_text(stream):
     response_text = ""
@@ -13,75 +21,53 @@ def extract_response_text(stream):
     return response_text
 
 
-def summarize_transcript(transcript, title=None, description=None):
-    if not transcript:
-        return "Error: Transcript is unavailable. Cannot generate a summary."
-
-    cleaned_transcript = transcript.replace("\n", " ")
-    cleaned_transcript = " ".join(cleaned_transcript.splitlines())
-
-    context = ""
-    if title:
-        context += f"Video title: {title}\n"
-    if description:
-        context += f"Video description: {description}\n"
-
-    prompt = (
-            "Summarize the following video in **English**, even if the transcript is in another language. "
-            "Translate key points if needed and ensure clarity and coherence. "
-            "Keep the summary **concise, structured, and under 500 words**:\n\n"
-            "1. **Introduction**: Briefly introduce the topic and its importance.\n"
-            "2. **Key Points**: Highlight the main insights.\n"
-            "3. **Conclusion**: Summarize key takeaways.\n\n"
-            f"**Video Context**:\n{context}\n\n"
-            f"**Transcript**:\n{cleaned_transcript}\n\n"
-        )
-    logger.info(f"Prompt for summary: {prompt[:1000]}")
-
-    stream = ollama.chat(
-        model="deepseek-r1",
-        messages=[{'role': 'user', 'content': prompt}],
-        stream=True,
-    )
-    response_text = extract_response_text(stream)
-
-    return response_text
-
-
-
 def generate_blog_post(transcript, title=None, description=None):
 
-    cleaned_transcript = transcript.replace("\n", " ")
-    cleaned_transcript = " ".join(cleaned_transcript.splitlines())
-    context = ""
-    if title:
-        context += f"Video title: {title}\n"
-    if description:
-        context += f"Video description: {description}\n"
+    try:
+        # Create a temporary file to save the transcript
+        with tempfile.NamedTemporaryFile('w', suffix='.txt', delete=False) as temp:
+            temp.write(transcript)
+            temp_path = temp.name
 
-    prompt = (
-        "Write a **technical blog post in English** for Medium based on the provided video context and transcript. "
-        "If the transcript is in another language, **extract key points and translate them into English** before writing the article. "
-        "The article should be **clear, concise, and engaging** for developers and tech companies, ensuring technical accuracy "
-        "while explaining complex concepts in an accessible way. Use a structured, logical flow with practical examples where relevant. "
-        "Include a **call to action** to encourage reader engagement. Keep it **professional yet approachable**.\n\n"
-        "Structure:\n"
-        "- **Introduction**: Briefly introduce the topic and why it matters.\n"
-        "- **Problem Statement**: Describe the issue faced.\n"
-        "- **Solution**: Explain how it was resolved with key details.\n"
-        "- **Challenges & Learnings**: Highlight obstacles and takeaways.\n"
-        "- **Relevance to the Reader**: Connect the topic to real-world applications.\n"
-        "- **Conclusion & Next Steps**: Summarize key points and suggest further reading.\n\n"
-        f"**Video Context**:\n{context}\n\n"
-        f"**Transcript** (can be in another language):\n{cleaned_transcript}\n\n"
-    )
+        context = ""
+        if title:
+            context += f"Video title: {title}\n"
+        if description:
+            context += f"Video description: {description}\n"
 
-    logger.info(f"Prompt for blog: {prompt[:1000]}")
-    stream = ollama.chat(
-        model="deepseek-r1",
-        messages=[{'role': 'user', 'content': prompt}],
-        stream=True,
-    )
-    response_text = extract_response_text(stream)
+        prompt = (
+            "Write a **technical blog post in English** for Medium based on the provided video context and transcript. "
+            "The transcript and context is provided in **English, Spanish, or Portuguese**. **First, translate them into English**, "
+            "then write the article based on that. "
+            "The article should be **clear, concise, and engaging** for developers and tech companies, ensuring technical accuracy "
+            "while explaining complex concepts in an accessible way. The post must be between **1000 and 1500 words**. "
+            "Use a structured, logical flow with practical examples where relevant. Include a **call to action** to encourage reader engagement. "
+            "Keep it **professional yet approachable**.\n\n"
+            "Structure:\n"
+            "- **Introduction**: Briefly introduce the topic and why it matters.\n"
+            "- **Problem Statement**: Describe the issue faced.\n"
+            "- **Solution**: Explain how it was resolved with key details.\n"
+            "- **Challenges & Learnings**: Highlight obstacles and takeaways.\n"
+            "- **Relevance to the Reader**: Connect the topic to real-world applications.\n"
+            "- **Conclusion & Next Steps**: Summarize key points and suggest further reading.\n\n"
+            f"**Video Context**:\n{context}\n\n"
+            f"*Transcript:\n{transcript}\n\n"
+        )
+
+        logger.info(f"Prompt for blog: {prompt[:1000]}")
+        stream = ollama.chat(
+            model=MODEL_TO_USE,
+            messages=[{'role': 'user', 'content': prompt}],
+            stream=True,
+        )
+        response_text = extract_response_text(stream)
+        response_text = re.sub(r'<think>.*?</think>', '', response_text, flags=re.DOTALL)
+
+    except Exception as e:
+        logger.error(f"Error generating blog post: {e}")
+        return "Error: Failed to generate a blog post. Please try again later."
+    finally:
+        # Delete the temporary file
+        os.remove(temp_path)
 
     return response_text
